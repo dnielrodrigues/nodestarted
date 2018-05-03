6eec360c32a5e0c3b5f448c113026617eb113165
Daniel Rodrigues <danielrodrigues.a113@gmail.com>
2018-05-03 19:35:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ---
+
+´´´
+´´´
+
+Dependências Iniciais:  
+´´´
+npm install --save express
+sudo npm install -g nodemon
+´´´
+
+
+Import básico:  
+´´´
+var express = require('express');
+var app = express();
+´´´
+
+
---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * legend
+ * -----------------------------------------------------------------------------
+ * 
+ * app: 		aplicacao principal
+ * req: 		request
+ * res: 		response
+ * 
+ * 
+ */
+
+
+
+// dependencias
+var express = require('express');
+var app = express();
+
+
+// host
+app.listen(3000,function(){
+	console.log('Rordando...');
+});
+
+
+// routes
+app.get('/teste',function(req,res){
+	console.log('rota: /teste');
+	res.send('OK...');
+});
