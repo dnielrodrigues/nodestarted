39f5d7eff719d75100dcaf3b958cf3abc490e3b1
Dani Rodrigues <daniel.rodrigues@aspec.com.br>
2025-08-01 23:25:32 -0300

    base module

--- src/modules/base/base.controller.ts
+++ src/modules/base/base.controller.ts
@@ -1,12 +1,66 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { BaseService } from './base.service'
 
-@Controller(':slug')
+@Controller(':model')
 export class BaseController {
   constructor(private readonly baseService: BaseService) {}
 
   @Get()
-  getModel(@Param('slug') slug: string): { welcome: string } {
-    return this.baseService.getModel(slug)
+  list(@Param('model') model: string): any {
+    // TODO - middleware
+    try {
+      this.baseService.setModel(model)
+      return this.baseService.list()
+    } catch (error) {
+      return error.name
+    }
+  }
+
+  @Get(':id')
+  get(@Param() { model, id }: { model: string; id: string }): any {
+    // TODO - middleware
+    try {
+      this.baseService.setModel(model)
+      return this.baseService.get(id)
+    } catch (error) {
+      return error.name
+    }
+  }
+
+  @Post()
+  create(@Param('model') model: string, @Body() body: any) {
+    // TODO - middleware
+    try {
+      this.baseService.setModel(model)
+      return this.baseService.save(body)
+    } catch (error) {
+      return error.name
+    }
+  }
+
+  @Put(':id')
+  update(
+    @Param() { model, id }: { model: string; id: string },
+    @Body() body: any
+  ): any {
+    // TODO - middleware
+    try {
+      const obj = { ...body, id }
+      this.baseService.setModel(model)
+      return this.baseService.save(obj)
+    } catch (error) {
+      return error.name
+    }
+  }
+
+  @Delete(':id')
+  delete(@Param() { model, id }: { model: string; id: string }): any {
+    // TODO - middleware
+    try {
+      this.baseService.setModel(model)
+      return this.baseService.delete(id)
+    } catch (error) {
+      return error.name
+    }
   }
 }
--- src/modules/base/base.service.ts
+++ src/modules/base/base.service.ts
@@ -2,7 +2,45 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class BaseService {
-  getModel(model: string): { welcome: string } {
-    return { welcome: model }
+  model?: string
+
+  // readers
+  get(id: string) {
+    return { model: this.model, id }
+  }
+
+  list(opt?: object) {
+    return { model: this.model, opt }
+  }
+
+  // actions
+  save(obj: any) {
+    return { model: this.model, obj }
+  }
+
+  delete(id: string) {
+    return { model: this.model, id }
+  }
+
+  // events
+  beforeSave(obj: any) {
+    return obj
+  }
+
+  afterSave(obj: any) {
+    return obj
+  }
+
+  beforeDelete(obj: any) {
+    return obj
+  }
+
+  afterDelete(obj: any) {
+    return obj
+  }
+
+  // getters/setters
+  setModel(str) {
+    if (str) this.model = str
   }
 }
